75b6d8b3bf9d74ee1a39a49669e97adc8630d434
Ferchisx <fefernandeza@unal.edu.co>
2024-09-07 10:30:10 -0500

    Project created
    Initialized Microchip Studio and created the folder where all the Hist Band project will be

---
+++ Hist_Band/Hist_Band/main.c
@@ -0,0 +1,18 @@
+/*
+ * Hist_Band.c
+ *
+ * Created: 7/9/2024 10:28:43
+ * Author : Felipe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
